3c015eca5061a17bb8b19ac9a7b02d8bd97207cd
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2022-03-06 20:08:44 -0500

    Update win-passless.bat

--- assessment-prep/win-passless.bat
+++ assessment-prep/win-passless.bat
@@ -1,1 +1,7 @@
 ssh-keygen -b 4096
+
+# C:\Users\WINUSER/.ssh/id_rsa.pub
+
+cp C:\Users\quentin\.ssh\id_rsa.pub C:\Users\quentin\authorized_keys
+cd C:\Users\quentin\
+scp authorized_keys quentin@[ubuntu-Host-Ip]:~/.ssh
