7435817f67a7cb666b4507afaaf7b55d31f71f19
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2022-04-05 16:38:55 -0400

    Create zeek.bash

---
+++ zeek.bash
@@ -0,0 +1,6 @@
+echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+
+curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+
+sudo apt update
+sudo apt install zeek
